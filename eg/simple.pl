50fbb81f6c51e14688a8e316d5cf15dee1b8ffe1
Delon Newman <delon.newman@gmail.com>
2011-10-24 22:46:31 -1000

    added events

--- eg/simple.pl
+++ eg/simple.pl
@@ -1,7 +1,21 @@
 #!/usr/bin/env perl 
 use v5.10;
+
+use lib qw{ lib };
 use Periscope;
+use Data::Dump qw(dump);
 
 die "Usage: $0 URL" unless @ARGV;
 
-Periscope->new(address => $ARGV[0])->show;
+my $p = Periscope->new(address => $ARGV[0]);
+$p->event('download-requested' => sub {
+	my $view     = shift;
+	my $download = shift;
+	my $url      = $download->get_uri;
+
+	say "downloading $url...";
+	system("wget $url");
+
+	return FALSE;
+});
+$p->show;
